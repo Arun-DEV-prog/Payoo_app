f9da3797aa626b371be7a2493cbca3b32e5fbb0c
Arun_DEV <arunkumarroy.cse2021@gmail.com>
2025-02-25 13:07:04 +0600

    cash out functional add

--- js/add_money.js
+++ js/add_money.js
@@ -21,3 +21,38 @@ add_Btn.addEventListener('click',(event)=>{
    }
 
 })
+
+
+//  Cash-out toggle
+const cash_out_btn=document.getElementById('cash-out-btn');
+const add_money_btn=document.getElementById('add-money-btn');
+cash_out_btn.addEventListener('click',()=>{
+    const add_money=document.getElementById("add-money")
+   const cash_out_money=document.getElementById("cash-out")
+     cash_out_money.style.display='block';
+     add_money.style.display="none";
+})
+add_money_btn.addEventListener('click',()=>{
+    const add_money=document.getElementById("add-money")
+   const cash_out_money=document.getElementById("cash-out")
+      add_money.style.display='block';
+      cash_out_money.style.display='none';
+})
+
+
+// Cash -out money charge
+
+const cashOutBtn=document.getElementById("cash-out-Btn");
+
+cashOutBtn.addEventListener('click',(event)=>{
+  event.preventDefault();
+    const balance=document.getElementById('balance').innerText;
+    const convertBalance=parseFloat(balance)
+
+     const cash_out_amount=document.getElementById('cash-out-amount').value;
+      const convetedCashOutAmount=parseFloat(cash_out_amount);
+      
+      const sub=convertBalance-convetedCashOutAmount;
+
+      document.getElementById('balance').innerText=sub;
+})
